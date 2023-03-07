6e0202f464381cf83e72686587cd3de6cd712f86
patrosk <nema@itiden.se>
2023-03-07 17:00:52 +0100

    add footer component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import './App.css';
 import Header from './Header.js';
 import useFetch from './useFetch';
 import ImgWrapper from './ImgWrapper';
+import Footer from './Footer';
 
 function App() {
 
   const { data, loading, error } = useFetch('https://api.quotable.io/random?tags=motivational');
 
-  if(loading) return <h2>loading...</h2>
+  if(loading) return (
+    <div className="App App-header">
+      <Header/>
+      <h2>loading...</h2>
+    </div>
+  );
   if(error) console.log(error);
 
   return (
     <div className="App App-header">
         <Header />
         <ImgWrapper/>
-        <h2>Quote:</h2>
+        <h5>This pupper wants you to know:</h5>
         <p>
           {data?.content}
         </p>
+        <Footer/>
     </div>
   );
 }
