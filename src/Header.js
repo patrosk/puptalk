d84c43f81791a9be67d5f8f0e7437239ed3ad449
patrosk <nema@itiden.se>
2023-03-08 11:37:26 +0100

    add logo to page and remove unused assets

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import img from './img/love.png';
 
 export default function Header() {
     return(
     <header>
+        <img className="logo" src={img} alt="Puptalk logo"></img>
         <h1>Puptalk</h1>
     </header>
     )
