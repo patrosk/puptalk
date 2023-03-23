df7ef82d66fac51702e7c71b9a4cdbdd2a2d70dd
patrosk <nema@itiden.se>
2023-03-23 12:38:45 +0100

    add placeholder image and comments to PupperImages.js

--- src/ImgWrapper.js
+++ src/ImgWrapper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import pupsArray from './PupperImages.js';
+import placeholder from './placeholderpuppy.jpg';
 
 const randomNumber = Math.floor(Math.random() * pupsArray.length);
 const randomPuppy = pupsArray[randomNumber];
@@ -8,7 +9,11 @@ export default function ImgWrapper() {
 
     return (
         <div className="img-wrapper">
+            {randomPuppy ? 
             <img className="pup-img" src={randomPuppy} alt="puppy"></img>
+            :
+            <img className="pup-img" src={placeholder} alt="puppy"></img>
+            }
         </div>
     )
 }
--- src/PupperImages.js
+++ src/PupperImages.js
@@ -1,13 +1,25 @@
+// Use webpack to import all images in a directory
+// save them temporarily in cache object
 const cache = {};
 
+// importAll function takes a require.context() as an argument
+// and loops through all the files in the directory
+// and saves them in the cache object
 function importAll(request) {
     request.keys().forEach((key) => (cache[key] = request(key)));
 }
+
+// run the importAll function on the images directory
+// fetch all files with .jpg extension
 importAll(require.context("../public/img", false, /\.jpg$/));
 
+// access the contents of the cache object
 const images = Object.entries(cache);
+
+// create an empty array
 var pupsArray = [];
 
+// loop through the images array and populate the empty array
 images.map(image => (
     pupsArray.push(image[1])
 ))
